7a5b01d5f9dc74bebfdff9a499e21a2b9f0beb38
larissabenedet <larissavb2014@outlook.com>
2021-06-19 14:13:30 -0300

    Jquery about

--- scripts.js
+++ scripts.js
@@ -1,11 +1,30 @@
-$( document ).ready(function() {
-    $(".products-hover").hide();
-    // $(".texto-gallery").hide();
+$(document).ready(function () {
+  $(".products-hover").hide();
+  $(".gallery-fotos-ny").hide();
+  $(".gallery-fotos-utica").hide();
 });
 
-$(".index-products-img").mouseover(function() {
-    $(".products-hover").fadeIn("fast");
-  });
-$(".index-products-img").mouseleave(function() {
-    $(".products-hover").fadeOut("fast");
-  });
+$(".index-products-img").mouseover(function () {
+  $(".products-hover").fadeIn("fast");
+});
+$(".index-products-img").mouseleave(function () {
+  $(".products-hover").fadeOut("fast");
+});
+
+$(".gallery-menu").click(function () {
+  $(".gallery-menu").removeClass("active");
+  $(this).addClass("active");
+});
+
+$("#ny").click(function () {
+  $(".gallery-all").slideUp("fast");
+  $(".gallery-fotos-ny").slideDown();
+});
+$("#scranton").click(function () {
+  $(".gallery-all").slideUp("fast");
+  $(".gallery-fotos-scranton").slideDown();
+});
+$("#utica").click(function () {
+  $(".gallery-all").slideUp("fast");
+  $(".gallery-fotos-utica").slideDown();
+});
